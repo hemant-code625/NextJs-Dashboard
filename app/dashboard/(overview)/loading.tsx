d69de5ee2824db4bb99349321ed91e2019f69805
hemant-code625 <hemantsinghparmar@mgmtech.org>
2024-03-21 09:08:32 +0530

    streaming ✅

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,13 @@
+import DashboardSkeleton from '@/app/ui/skeletons'
+import React from 'react'
+
+
+const loading = () => {
+  return (
+    <>
+     <DashboardSkeleton/> 
+    </>
+  )
+}
+
+export default loading
